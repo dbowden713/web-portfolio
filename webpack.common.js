a72ef80453079e108333f04a9038014cad170a5e
Daniel Bowden <dbowden713@gmail.com>
2020-01-29 02:14:32 -0600

    Added react and babel

--- webpack.common.js
+++ webpack.common.js
@@ -6,6 +6,11 @@ module.exports =  {
     module: {
         rules: [
             {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"]
+            },
+            {
                 test: /\.html$/,
                 use: ["html-loader"]
             },
